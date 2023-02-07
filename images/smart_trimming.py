762337dffc602ab3a27a05a147119ad148b8a391
Ardisat <83712099+Ardisat@users.noreply.github.com>
2023-02-07 15:55:16 +0600

    last update

--- images/smart_trimming.py
+++ images/smart_trimming.py
@@ -1,38 +1,20 @@
 from PIL import Image
 
 
-def smart_trimming(img, size=(100, 100)):
+def smart_trimming(img, target_size):
+    if img.width < target_size[0]:
+        new_height = int(target_size[0] * img.height / img.width)
+        img = img.resize((target_size[0], new_height), Image.ANTIALIAS)
 
-    # In height
-    if img.width < size[0]:
-        new_height = int(size[0] * img.height / img.width)
-        img = img.resize((size[0], new_height), Image.ANTIALIAS)
+    if img.height < target_size[1]:
+        new_width = int(img.width * target_size[1] / img.height)
+        img = img.resize((new_width, target_size[1]), Image.ANTIALIAS)
 
-    # In width
-    if img.height < size[1]:
-        new_width = int(img.width * size[1] / img.height)
-        img = img.resize((new_width, size[1]), Image.ANTIALIAS)
+    new_img = Image.new("RGB", target_size, (255, 255, 255))
 
-    # Reducing large
-    if img.width > size[0] and img.height > size[1]:
+    left = (target_size[0] - img.width) // 2
+    top = (target_size[1] - img.height) // 2
 
-        # In height
-        if img.width > img.height:
-            new_height = int(size[0] * img.height / img.width)
-            img = img.resize((size[0], new_height), Image.ANTIALIAS)
-        else:
-            # In width
-            new_width = int(img.width * size[1] / img.height)
-            img = img.resize((new_width, size[1]), Image.ANTIALIAS)
-
-    # Creating a new image
-    new_img = Image.new("RGB", size, (255, 255, 255))
-
-    # Defining box by center
-    left = (size[0] - img.width) // 2
-    top = (size[1] - img.height) // 2
-
-    # Pasting box on new image
     new_img.paste(img, (left, top))
 
     return new_img
